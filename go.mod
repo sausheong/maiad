01e1241690acf08d8fd4b892b73b8b380e648ee0
Chang Sau Sheong <sausheong@gmail.com>
2023-02-20 21:43:44 +0800

    removed env

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/sausheong/goair
 
 go 1.19
-
-require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -5,16 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
-
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Printf("Failed to load the env vars: %v", err)
-	}
-}
 
 func main() {
 
