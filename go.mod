c806daf9b8723f1f93de6cd4d198dadd38ad8983
Chang Sau Sheong <sausheong@gmail.com>
2023-02-23 00:05:06 +0800

    added connection to OpenAI

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sausheong/goair
 
 go 1.19
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sausheong/openai v0.0.0-20230222154925-381e26bb4b69
+)
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/sausheong/openai"
 )
 
 func init() {
@@ -41,9 +43,34 @@ func generate(w http.ResponseWriter, r *http.Request) {
 	data := string(body)
 	log.Println(data)
 
-	w.Write([]byte(data))
+	text, err := talkToOpenAI("tell me more about ", data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Write([]byte(text))
 }
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+// talk to OpenAI API
+func talkToOpenAI(header string, prompt string) (string, error) {
+	openAIApiKey := os.Getenv("OPENAI_API_KEY")
+	openAIOrganization := os.Getenv("OPENAI_ORGANIZATION")
+
+	oaClient := openai.NewClient(openAIApiKey, openAIOrganization)
+	request := make(openai.CompletionRequest)
+	request.SetUser("sausheong")
+	request.SetModel(openai.TEXT_DAVINCI_002)
+	request.SetPrompt(fmt.Sprintf("%s:%s", header, prompt))
+	request["temperature"] = 0.75
+	request["max_tokens"] = 50
+
+	cr, err := oaClient.Complete(request)
+	if err != nil {
+		log.Println("Completion request failed:", err)
+	}
+	return cr.Text(), err
 }
