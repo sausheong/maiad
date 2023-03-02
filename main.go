7638dbe7b060cc9bc956e481a9dc7fc2eabb5571
Chang Sau Sheong <sausheong@gmail.com>
2023-03-02 21:28:24 +0800

    switched back to davinci

--- main.go
+++ main.go
@@ -82,7 +82,7 @@ func enableCors(w *http.ResponseWriter) {
 func ask(prompt string) (string, error) {
 	oaClient := openai.NewClient(openAIApiKey, openAIOrganization)
 	request := make(openai.CompletionRequest)
-	request.SetModel(openai.TEXT_GPT_35_TURBO)
+	request.SetModel(openai.TEXT_DAVINCI_003)
 	request.SetPrompt(prompt + " {}")
 	request["temperature"] = 0.75
 	request["max_tokens"] = 4096 - len(prompt)
